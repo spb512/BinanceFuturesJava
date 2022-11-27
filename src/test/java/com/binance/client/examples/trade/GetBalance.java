1b5977e88cd97551f380d0e785829c574d7acafc
spb512@163.com <spb512@163.com>
2022-11-27 13:15:04 +0800

    1、升级依赖。

--- src/test/java/com/binance/client/examples/trade/GetBalance.java
+++ src/test/java/com/binance/client/examples/trade/GetBalance.java
@@ -3,13 +3,19 @@ package com.binance.client.examples.trade;
 import com.binance.client.RequestOptions;
 import com.binance.client.SyncRequestClient;
 import com.binance.client.examples.constants.PrivateConfig;
+import com.binance.client.exception.BinanceApiException;
 
 public class GetBalance {
     public static void main(String[] args) {
         RequestOptions options = new RequestOptions();
         options.setUrl("https://testnet.binancefuture.com");
-        SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
-                options);
-        System.out.println(syncRequestClient.getBalance());
+        try {
+            SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
+                    options);
+            System.out.println(syncRequestClient.getBalance());
+        } catch (BinanceApiException e){
+            System.out.println(e.getErrType());
+            System.out.println(e.getMessage().split(":")[0]);
+        }
     }
 }
